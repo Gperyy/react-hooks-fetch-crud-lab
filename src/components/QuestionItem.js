d1e7edd424f633099f99b5a726336cb6643fce7b
Gulperi Ozbulut <gulperiozblt@gmail.com>
2023-06-25 18:33:21 +0300

    Completed assignment

--- src/components/QuestionItem.js
+++ src/components/QuestionItem.js
@@ -1,25 +1,49 @@
 import React from "react";
 
-function QuestionItem({ question }) {
-  const { id, prompt, answers, correctIndex } = question;
+function QuestionItem({ question, onDeleteQuestion, onUpdateQuestion }) {
+    const { id, prompt, answers, correctIndex } = question;
 
-  const options = answers.map((answer, index) => (
-    <option key={index} value={index}>
-      {answer}
-    </option>
-  ));
+    const options = answers.map((answer, index) => (
+        <option key={index} value={index}>
+            {answer}
+        </option>
+    ));
 
-  return (
-    <li>
-      <h4>Question {id}</h4>
-      <h5>Prompt: {prompt}</h5>
-      <label>
-        Correct Answer:
-        <select defaultValue={correctIndex}>{options}</select>
-      </label>
-      <button>Delete Question</button>
-    </li>
-  );
+    function handleUpdateClick(event) {
+        fetch(`http://localhost:4000/questions/${id}`, {
+            method: "PATCH",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+                correctIndex: event.target.value,
+            }),
+        })
+            .then((r) => r.json())
+            .then((updatedQuestion) => onUpdateQuestion(updatedQuestion));
+    }
+
+    function handleDeleteClick() {
+        fetch(`http://localhost:4000/questions/${id}`, {
+            method: "DELETE",
+        })
+            .then((r) => r.json())
+            .then(() => onDeleteQuestion(question));
+    }
+
+    return (
+        <li>
+            <h4>Question {id}</h4>
+            <h5>Prompt: {prompt}</h5>
+            <label>
+                Correct Answer:
+                <select defaultValue={correctIndex} onChange={handleUpdateClick}>
+                    {options}
+                </select>
+            </label>
+            <button onClick={handleDeleteClick}>Delete Question</button>
+        </li>
+    );
 }
 
 export default QuestionItem;
